c61988bcd5289c465920963b51c04d92375b8f04
Ruta Basijokaite <111301407+RutaCodes@users.noreply.github.com>
2022-09-06 14:25:07 -0400

    Update and rename Basijokaite_HW3_Fun.R to LN3_LP3_Fun.R

--- Coursework_HydrologicModeling/LinearRegressionWithLeaps/LN3_LP3_Fun.R
+++ Coursework_HydrologicModeling/LinearRegressionWithLeaps/LN3_LP3_Fun.R
@@ -3,11 +3,10 @@ LN3 <- function(mini, perc){
   #Zp
   zp<-qnorm(perc)
   if (min(mini)+max(mini)-2*median(mini)<=0) { 
-    
     Y=log(mini)
     N=length(mini)
-    Y_bar=mean(Y)#Y_bar=sum(log(mini))/length(mini)
-    Sy=sd(Y)#Sy=sqrt(sum((log(mini)-Y_bar)^2))/(length(mini)-1)
+    Y_bar=mean(Y)
+    Sy=sd(Y)
     Q7_10=exp(Y_bar+zp*Sy)
   }
   else {
@@ -15,9 +14,8 @@ LN3 <- function(mini, perc){
     LB_est<-(min(mini)*max(mini)-median(mini)^2)/(min(mini)+max(mini)-(2*median(mini)))
     #Y
     Y_ap=log(mini-LB_est)
-    #N_ap=length(Y_ap)
-    Y_bar=mean(Y_ap) #Y_bar=sum(Y_ap)/length(Y_ap)
-    Sy=sd(Y_ap) #Sy=sum((Y_ap-Y_bar)^2)/(length(Y_ap)-1)
+    Y_bar=mean(Y_ap) 
+    Sy=sd(Y_ap) 
     #100 year flood
     Q7_10=LB_est+exp(Y_bar+zp*Sy)
   }
@@ -29,8 +27,8 @@ LP3<-function(maxi,per){
   zp<-qnorm(per)
   Y<-log(maxi)
   N=length(maxi)
-  Y_bar<-mean(Y) # Y_bar=sum(log(x_dis))/N
-  Sy<-sd(Y) # Sy=sqrt(sum((log(x_dis)-Y_bar)^2)/(N-1))
+  Y_bar<-mean(Y) 
+  Sy<-sd(Y) 
   Gy=(N*sum((Y-Y_bar)^3))/((N-1)*(N-2)*Sy^3)
   Gy_ap=(1+6/N)*Gy
   Kp=(2/Gy_ap)*((1+(zp*Gy_ap/6)-((Gy_ap^2)/36))^3)-(2/Gy_ap)
