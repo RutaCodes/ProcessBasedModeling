3fb59f2fcf252e866a279b2fa6f6054ea95b3e80
Ruta Basijokaite <111301407+RutaCodes@users.noreply.github.com>
2022-09-12 13:16:04 -0400

    Add files via upload

---
+++ Coursework_HydrologicModeling/ModelSensitivity/Par_Change_SA.R
@@ -0,0 +1,106 @@
+HW_8_look <- function (Pars){
+  
+  Given_CN2=Pars[1]
+  Kb=Pars[2]
+  M=Pars[3]
+  
+  Climate<-read.csv(file="Climate_data.csv",sep=",",header=T)
+  Genesee_data<-read.csv(file="Genesee_data.csv",sep=",",header=F)
+  Discharge_obs<-Genesee_data[,2]
+  date=as.POSIXlt(Genesee_data[,1], format="%m/%d/%y")
+  len=length(Discharge_obs)
+  
+  #Values from HW6
+  Data_Roch<-read.csv(file="Rochester.csv",sep=",",header=T)
+  Data_Buff<-read.csv(file="Buffalo.csv",sep=",",header=T)
+  source("HW6_fun.R")
+  HW6_out=HW6_loop(Climate,Data_Roch,Data_Buff)
+  
+  #Values from HW5
+  source("HW5_fun.R")
+  HW5_out=HW5_loop(HW6_out,date)
+  Q_cfs=HW5_out[,1]
+  ET=M*HW5_out[,2] # add in ET multiplier
+  
+  CN1=Given_CN2/(2.334-0.01334*Given_CN2)
+  CN3=Given_CN2/(0.4036+0.0059*Given_CN2)
+  
+  #Determining AM1 and AM2 values
+  A1=c(1.3,1.3,1.3,1.3,3.6,3.6,3.6,3.6,3.6,3.6,1.3,1.3)
+  A2=c(2.8,2.8,2.8,2.8,5.3,5.3,5.3,5.3,5.3,5.3,2.8,2.8)
+  
+  P_val=HW6_out[,3]*2.54 #in cm
+  AM5=0 #Precipitation on 5 previous days
+  AM5[1:len]=0
+  AM5[2]=P_val[1]
+  AM5[3]=P_val[1]+P_val[2]
+  AM5[4]=P_val[1]+P_val[2]+P_val[3]
+  AM5[5]=P_val[1]+P_val[2]+P_val[3]+P_val[4]
+  for (i in seq(6,len)){
+    for (j in seq(1,5)){
+      AM5[i]=AM5[i]+P_val[i-j]
+    }
+  }
+  
+  CN=0
+  CN[1:len]=0
+  for (i in seq(1,len)){
+    mo_nr=date$mon[i]+1
+    if (AM5[i]<A1[mo_nr]){
+      CN[i]=CN1+((Given_CN2-CN1)/(A1[mo_nr]-0))*AM5[i]
+    }
+    if (A1[mo_nr]<=AM5[i] & AM5[i]<=A2[mo_nr]){
+      CN[i]=Given_CN2+((CN3-Given_CN2)/(A2[mo_nr]-A1[mo_nr]))*(AM5[i]-A1[mo_nr])
+    }
+    if (AM5[i]>A2[mo_nr]){
+      CN[i]=CN3
+    } 
+  }
+  
+  S=(2540/CN)-25.4
+  
+  #P_out=data.frame(date,P_val,AM5)
+  
+  #IF P is < than 0.2*S, then Runoff=0
+  SR=0
+  I=0
+  SR[1:len]=0
+  I[1:len]=0
+  for(i in seq(1,len)){
+    if (P_val[i]<0.2*S[i]){
+      SR[i]=0
+    }else{
+      SR[i]=((P_val[i]-0.2*S[i])^2)/(P_val[i]+0.8*S[i])
+    }
+    I[i]=P_val[i]-SR[i]
+  }
+  
+  #Kb=0.9
+  SS=0
+  SD=0
+  SD[1:len]=0
+  SS[1:len]=0
+  SS[1]=4.04 #cm
+  for (i in seq(2,len)){
+    if (SS[i-1]<ET[i-1]){
+      SS[i]=I[i-1]
+      SD[i-1]=0
+    }
+    else{
+      SD[i-1]=(1-Kb)*(SS[i-1]-ET[i-1])
+      SS[i]=SS[i-1]+I[i-1]-ET[i-1]-SD[i-1]
+    }
+  }
+  SD[len]=0
+  
+  #Calculated Q
+  Q_pred=0
+  Q_pred[1:len]=0
+  for(i in seq(1,len)){
+    Q_pred[i]=SR[i]+SD[i] #cm/day
+  }
+  Q_cfs_new=Q_pred*(984*2.59*10^10)*4.08734569*10^(-10)
+  #mi2 to cm2       to cfs
+  
+  return(Q_cfs_new)
+}
---
+++ Coursework_HydrologicModeling/ModelSensitivity/Sens_anal.R
@@ -0,0 +1,127 @@
+#Ruta Basijokaite
+
+#----------------
+#This code performs a global sensitivity analysis using the Morris One-factor-At-a-Time (OAT) method on SCS Model to 
+#analyze its parameter sensitivity. Parameters analyzed: CN, Kb, M(multiplier of ET).
+#Elementary effect (EE) for each parameter is analyzed, where the model output (y) is the NSE of the monthly average 
+#streamflows. p=5 (number of discretization of each parameter) and k=15 (number of repetitions) are used for OAT 
+#technique. 
+#----------------
+
+Genesee_data<-read.csv(file="Genesee_data.csv",sep=",",header=F)
+Discharge_obs<-Genesee_data[,2] #cfs
+
+#Calculating delta values for model parameters
+delta_CN=(90-25)/(5-1)
+delta_Kb=(0.99-0.8)/(5-1)
+delta_M=(1-0.2)/(5-1)
+
+CN_seq=seq(25,90, by=delta_CN)
+Kb_seq=seq(0.8,0.99, by=delta_Kb)
+M_seq=seq(0.2,1, by=delta_M)
+
+r_v=sample(1:4, 3, replace=T)
+Main_seq=c(CN_seq[r_v[1]],Kb_seq[r_v[2]],M_seq[r_v[3]])
+
+Seq_var=matrix(0,15,3)
+R_par=0
+R_seq=0
+for (i in seq(1,15)){
+  r_v_sample=sample(1:3, 1, replace=T)
+  if (r_v_sample==1){
+    A1=c(1:4)
+    A1_rem=A1[A1!=r_v[1]]
+    r_v_index=sample(A1_rem, 1, replace=T)
+    Seq_var[i,]=c(CN_seq[r_v_index],Kb_seq[r_v[2]],M_seq[r_v[3]])
+  }
+  if (r_v_sample==2){
+    A1=c(1:4)
+    A1_rem=A1[A1!=r_v[2]]
+    r_v_index=sample(A1_rem, 1, replace=T)
+    Seq_var[i,]=c(CN_seq[r_v[1]],Kb_seq[r_v_index],M_seq[r_v[3]])
+  }
+  if (r_v_sample==3){
+    A1=c(1:4)
+    A1_rem=A1[A1!=r_v[3]]
+    r_v_index=sample(A1_rem, 1, replace=T)
+    Seq_var[i,]=c(CN_seq[r_v[1]],Kb_seq[r_v[2]],M_seq[r_v_index])
+  }
+  R_par[i]=r_v_sample
+  R_seq[i]=r_v_index
+}
+
+source("Par_Change_SA.R") #function that use new parameter values as an input and returns Q simulated
+Q_par_val=matrix(0,7305,15)
+Q_par_stan=0
+Q_par_stan=HW_8_look(Main_seq)
+for (i in seq(1,15)){
+  Q_par_val[,i]=HW_8_look(Seq_var[i,])
+}
+source("Error_met_fun.R") #function that calculated montly and annual Bias and NSE
+error_met_new=0
+NSE_mon_new=0
+NSE_main=0
+error_main=NSE_Bias_loop(date,Discharge_obs,Q_par_stan)
+NSE_main=error_main[4]
+for (i in seq(1,15)){
+  error_met_new=NSE_Bias_loop(date,Discharge_obs,Q_par_val[,i])
+  NSE_mon_new[i]=error_met_new[4]
+}
+
+#elemantary effect for each parameter combination
+cn1=0
+cn2=0
+cn3=0
+Par_val=0
+delta=0
+xi=0
+SI_num=0
+SI_den=0
+SI_CN=0
+SI_Kb=0
+SI_M=0
+for (i in seq(1:15)){
+  if(R_par[i]==1){
+    Par_val[i]=CN_seq[R_seq[i]]
+    delta[i]=abs(Par_val-Main_seq[1])
+    xi[i]=Main_seq[1]
+    cn1=cn1+1
+    SI_num=(abs(NSE_mon_new[i])-abs(NSE_main))/abs(NSE_main)
+    SI_den=delta/xi
+    SI_CN[cn1]=SI_num/SI_den
+  }
+  if(R_par[i]==2){
+    Par_val[i]=Kb_seq[R_seq[i]]
+    delta[i]=abs(Par_val-Main_seq[2])
+    xi[i]=Main_seq[2]
+    cn2=cn2+1
+    SI_num=(abs(NSE_mon_new[i])-abs(NSE_main))/abs(NSE_main)
+    SI_den=delta/xi
+    SI_Kb[cn2]=SI_num/SI_den
+  }
+  if(R_par[i]==3){
+    Par_val[i]=M_seq[R_seq[i]]
+    delta[i]=abs(Par_val-Main_seq[3])
+    xi[i]=Main_seq[3]
+    cn3=cn3+1
+    SI_num=(abs(NSE_mon_new[i])-abs(NSE_main))/abs(NSE_main)
+    SI_den=delta/xi
+    SI_M[cn3]=SI_num/SI_den
+  }
+}
+mean_CN=mean(SI_CN)
+std_CD=sd(SI_CN)
+mean_Kb=mean(SI_Kb)
+std_Kb=sd(SI_Kb)
+mean_M=mean(SI_M)
+std_M=sd(SI_M)
+
+means=c(mean_CN,mean_Kb,mean_M)
+stds=c(std_CD,std_Kb,std_M)
+
+#Plot with mean EE values on the y-axis and the standard deviation of EE on the x-axis for all three variables
+plot(stds[1],means[1],xlab=paste("St. Dev. EE"),ylab=paste("Mean EE"),xlim=c(min(stds),max(stds)),ylim=c(min(means),max(means)),main="Elementary Effect",pch=16,cex=0.75)
+points(stds[2],means[2],col="blue",pch=16,cex=0.75)
+points(stds[3],means[3],col="red",pch=16,cex=0.75)
+legend(x="bottomright",legend=c("CN","Kb","M"),col=c("black","blue","red"),pch=c(16,16,16),cex=0.6,bty = "n")
+#legend(x="topleft",legend=c("Ruta, Julianne, Mandy"),cex=0.65,bty = "n")
