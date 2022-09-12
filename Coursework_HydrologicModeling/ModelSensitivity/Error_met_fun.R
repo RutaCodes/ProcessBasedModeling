e77500b61823fd2fbb15e119e703a93529612825
Ruta Basijokaite <111301407+RutaCodes@users.noreply.github.com>
2022-09-12 13:19:44 -0400

    Update Error_met_fun.R

--- Coursework_HydrologicModeling/ModelSensitivity/Error_met_fun.R
+++ Coursework_HydrologicModeling/ModelSensitivity/Error_met_fun.R
@@ -1,3 +1,5 @@
+#Ruta Basijokaite
+
 NSE_Bias_loop<-function(date,Discharge_obs,Q_cfs){
 
 source("Average_fun.R")
@@ -6,7 +8,6 @@ Ann_obs=Annual_loop(date,Discharge_obs)
 Yearly_aver_obs=Ann_obs[,1]
 #Predicted Annual averages
 Ann_pred=Annual_loop(date,Q_cfs)
-#Ann_pred=Annual_loop(date,Q_cfs_calibrated)
 Yearly_aver_pred=Ann_pred[,1]
 
 #Monthly averages
@@ -29,10 +30,8 @@ for (i in seq (1,l)){
 #Bias and Nash Sutcliffe Efficiency
 source("Bias_NSE.R")
 bias_ann_v=Bias_v(Yearly_aver_pred,Yearly_aver_obs)
-#bias_mon_v=Bias_v(Aver_daily,Aver_daily_obs)
 bias_mon_v=Bias_v(Mon_aver,Mon_aver_obs)
 NSE_ann_v=NSE_v(Yearly_aver_pred,Yearly_aver_obs)
-#NSE_mon_v=NSE_v(Aver_daily,Aver_daily_obs)
 NSE_mon_v=NSE_v(Mon_aver,Mon_aver_obs)
 
 Bias_NSE_val=c(bias_ann_v,bias_mon_v,NSE_ann_v,NSE_mon_v)
