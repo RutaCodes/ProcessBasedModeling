1e1a6bfe94fa067ed97e95e19242f572ba58886e
Ruta Basijokaite <111301407+RutaCodes@users.noreply.github.com>
2022-09-17 22:16:19 -0400

    Update RegressionWithLeaps.R

--- Coursework_HydrologicModeling/LinearRegressionWithLeaps/RegressionWithLeaps.R
+++ Coursework_HydrologicModeling/LinearRegressionWithLeaps/RegressionWithLeaps.R
@@ -1,9 +1,9 @@
 #Ruta Basijokaite
-#ERE645 Hydrologic Modeling 
-
+#----------------
 #This code uses leaps to identify potential explanatory variables to create model for
 #predicting average streamflow, Q100 and 7Q10. Different models created are then
 #compared using various regression analysis techniques to find the best model
+#----------------
 
 #Variables:
 #Data_all - dataset
